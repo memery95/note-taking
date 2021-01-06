2748142d384790a66a6c24e14404e4c7f3d33750
Marcus Emery <marcusemery95@gmail.com>
2021-01-06 10:03:45 -0500

    config file change

--- src/config.js
+++ src/config.js
@@ -1,9 +1,3 @@
-// export default {
-//     API_ENDPOINT: 'http://localhost:9090'
-//   }
-  
-const config = {
-  API_ENDPOINT: 'http://localhost:9090'
-}
-
-export default config;
+export default {
+    API_ENDPOINT: 'http://localhost:9090'
+  }
